1ca981c846c7ef569bed7c507987c7f6a937e7c5
kahmariano <kmariano138@gmail.com>
2022-02-02 23:44:44 -0300

    second commit

---
+++ index.js
@@ -0,0 +1,13 @@
+alert('vc que mesmo saber?')
+
+let nowDate = new Date()
+document.write(
+  'Hoje é ' +
+    nowDate.getDate() +
+    ' de ' +
+    nowDate.getFullYear() +
+    ' e agora são ' +
+    nowDate.getHours() +
+    ':' +
+    nowDate.getMinutes()
+)
