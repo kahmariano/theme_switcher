a0885a5aa6701807318fc30c88a2000830a6e4e9
kahmariano <kmariano138@gmail.com>
2022-02-03 22:11:10 -0300

    three commit

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-alert('vc que mesmo saber?')
+//function para definir um determinado tema
 
-let nowDate = new Date()
-document.write(
-  'Hoje é ' +
-    nowDate.getDate() +
-    ' de ' +
-    nowDate.getFullYear() +
-    ' e agora são ' +
-    nowDate.getHours() +
-    ':' +
-    nowDate.getMinutes()
-)
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName)
+  document.documentElement.className = themeName
+}
+
+// function para alternar(toggle) entre tema claro e escuro
+//setTheme significa definir tema
+function toggleTheme() {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-dark')
+  } else {
+    setTheme('theme-ligth')
+  }
+}
+
+//function para invocar a definição do tema no carregamento inicial
+;(function () {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-dark')
+    document.getElementById('slider').checked = false
+  } else {
+    setTheme('theme-light')
+  }
+  document.getElementById('slider').checked = true
+})()
